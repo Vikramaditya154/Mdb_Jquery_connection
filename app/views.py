59880ce4f985a14a0af2cedcabd986e721c5620b
Vikramaditya154 <arjunroyal6385@gmail.com>
2023-06-12 20:00:56 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Bootstrap_Mdbootstrap(request):
+    return render(request,'Bootstrap&mdbootstarp.html')
+
+def bg_coonection(request):
+    return render(request,'back-ground-images.html')
+    
